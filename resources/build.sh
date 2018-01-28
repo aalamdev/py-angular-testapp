52d14c3bcbf6508501d628e94663704f6ede0c53
babu.shanmugam@gmail.com <babu@aalam.io>
2018-01-28 17:38:18 +0530

    Replaced the occurences of py_ang_testapp completely

--- resources/build.sh
+++ resources/build.sh
@@ -9,9 +9,9 @@ for f in `ls dist`; do
     fi
 done
 
-sed -i -e "s#href=\"styles#href=\"/aalam/py_ang_testapp/s/dist/styles#" dist/index.html
-sed -i -e "s#src=\"main#src=\"/aalam/py_ang_testapp/s/dist/main#" dist/index.html
-sed -i -e "s#src=\"inline#src=\"/aalam/py_ang_testapp/s/dist/inline#" dist/index.html
-sed -i -e "s#src=\"vendor#src=\"/aalam/py_ang_testapp/s/dist/vendor#" dist/index.html
-sed -i -e "s#src=\"polyfills#src=\"/aalam/py_ang_testapp/s/dist/polyfills#" dist/index.html
+sed -i -e "s#href=\"styles#href=\"/aalam/pyangtestapp/s/dist/styles#" dist/index.html
+sed -i -e "s#src=\"main#src=\"/aalam/pyangtestapp/s/dist/main#" dist/index.html
+sed -i -e "s#src=\"inline#src=\"/aalam/pyangtestapp/s/dist/inline#" dist/index.html
+sed -i -e "s#src=\"vendor#src=\"/aalam/pyangtestapp/s/dist/vendor#" dist/index.html
+sed -i -e "s#src=\"polyfills#src=\"/aalam/pyangtestapp/s/dist/polyfills#" dist/index.html
 ln -sf dist/index.html index.html
